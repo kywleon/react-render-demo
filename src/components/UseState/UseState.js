759ca5591f862145d4091d654c97143e0340ff4c
Kwok Yew Weng <66321555+kywleon@users.noreply.github.com>
2023-04-01 13:50:40 +0800

    Learn the react render

---
+++ src/components/UseState/UseState.js
@@ -0,0 +1,14 @@
+import React , { useState } from 'react'
+
+function UseState() {
+    const [count, setCount] = useState(0);
+
+    console.log(`UseState Render`)
+  return (
+    <div>
+        <button onClick={() => setCount(c => c + 1)}>Count - { count }</button>
+    </div>
+  )
+}
+
+export default UseState
